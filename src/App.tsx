b171c890d8e87c155625ee9e05441cdce3b52f36
KazukiHayase <k.hayase@buysell-technologies.com>
2024-01-01 15:18:57 +0900

    update directory

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+import { Tmp } from './components/Tmp';
+
+export default function App() {
+  return <Tmp />;
+}
+
+registerRootComponent(App);
