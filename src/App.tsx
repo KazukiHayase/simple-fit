142155baa87c949ee621415bb214f327f24cd4c9
KazukiHayase <k.hayase@buysell-technologies.com>
2024-01-01 15:36:50 +0900

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
-import { Tmp } from './components/Tmp';
+import { Tmp } from '@/components/Tmp';
 
 export default function App() {
   return <Tmp />;
