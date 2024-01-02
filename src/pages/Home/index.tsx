311d476d98bb26fffb6c0728cc3253e1e4ecd5f1
KazukiHayase <k.hayase@buysell-technologies.com>
2024-01-02 16:07:04 +0900

    add listItem

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,66 @@
-import { StyleSheet } from "react-native";
-import { Appbar, Text, useTheme } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Appbar, Divider, List, useTheme } from "react-native-paper";
 
 export const Home: React.FC = () => {
-	const theme = useTheme();
+	const styles = useStyle();
 	const today = "2021/08/01";
 
 	return (
 		<>
-			<Appbar.Header
-				style={[
-					styles.header,
-					{ borderBottomColor: theme.colors.surfaceVariant },
-				]}
-			>
-				<Appbar.Content
-					title={today}
-					titleStyle={{ fontSize: 18, fontWeight: "bold" }}
-				/>
+			<Appbar.Header style={styles.header}>
+				<Appbar.Content title={today} titleStyle={styles.headerTitle} />
 			</Appbar.Header>
-			<Text variant="displayMedium">Todo</Text>
+			<View style={styles.wrapper}>
+				<List.Section>
+					<List.Subheader style={styles.sectionHeader}>
+						今日のトレーニング
+					</List.Subheader>
+					{Array.from({ length: 5 }).map((_, i) => (
+						<>
+							<List.Item
+								title="ベンチプレス"
+								description="5セット"
+								right={(props) => <List.Icon {...props} icon="chevron-right" />}
+								titleStyle={{ fontWeight: "bold" }}
+								style={styles.listItem}
+								onPress={() => alert(`pressed ${i}`)}
+							/>
+							<Divider />
+						</>
+					))}
+				</List.Section>
+			</View>
 		</>
 	);
 };
 
-const styles = StyleSheet.create({
-	header: {
-		display: "flex",
-		alignItems: "center",
-		height: 40,
-		borderBottomWidth: 1,
-	},
-});
+const useStyle = () => {
+	const { colors } = useTheme();
+
+	return StyleSheet.create({
+		header: {
+			display: "flex",
+			alignItems: "center",
+			height: 40,
+			borderBottomWidth: 1,
+			borderBottomColor: colors.surfaceVariant,
+		},
+		headerTitle: {
+			fontSize: 18,
+			fontWeight: "bold",
+		},
+		wrapper: {
+			height: "100%",
+			padding: 16,
+			backgroundColor: colors.background,
+		},
+		sectionHeader: {
+			fontSize: 20,
+			fontWeight: "bold",
+		},
+		listItem: {
+			width: "100%",
+			paddingRight: 10,
+		},
+	});
+};
