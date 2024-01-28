15b305ee9e8784a5af22cc50a1701ba1e98002c9
KazukiHayase <k.hayase@buysell-technologies.com>
2024-01-29 00:09:46 +0900

    add nav icon

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,4 +1,5 @@
 import { Tabs } from "expo-router/tabs";
+import { Icon } from "react-native-paper";
 
 // TODO: typed router対応
 export const Navigation: React.FC = () => {
@@ -11,12 +12,18 @@ export const Navigation: React.FC = () => {
 				name="index"
 				options={{
 					title: "ホーム",
+					tabBarIcon: ({ color, size }) => (
+						<Icon source="home" color={color} size={size} />
+					),
 				}}
 			/>
 			<Tabs.Screen
 				name="record/index"
 				options={{
 					title: "記録",
+					tabBarIcon: ({ color, size }) => (
+						<Icon source="history" color={color} size={size} />
+					),
 				}}
 			/>
 		</Tabs>
