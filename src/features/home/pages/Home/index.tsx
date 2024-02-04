8092375b41bb3ca34d48f68afd2b24c6e9057db3
KazukiHayase <k.hayase@buysell-technologies.com>
2024-02-04 19:34:01 +0900

    use realm data

--- src/features/home/pages/Home/index.tsx
+++ src/features/home/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useQuery, useRealm } from "@/realm";
 import { Training } from "@/realm/model/Training";
+import dayjs from "dayjs";
 import { useEffect } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import { Divider, FAB, List, useTheme } from "react-native-paper";
@@ -9,28 +10,34 @@ export const Home: React.FC = () => {
 	const styles = useStyle();
 
 	const realm = useRealm();
-	// const profiles = useQuery(Training);
+
+	const trainings = useQuery(Training, (trainings) => {
+		const today = dayjs().locale("ja").toDate();
+		today.setHours(0, 0, 0, 0);
+		return trainings.filtered("createdAt >= $0", today);
+	});
 
 	// FIXME: 動作確認用データ投入
 	useEffect(() => {
 		realm.write(() => {
 			realm.deleteAll();
-			const type = realm.create("TrainingType", {
-				_id: new BSON.ObjectId(),
-				name: "ベンチプレス",
-				part: "CHEST",
-			});
+			for (let i = 0; i < 10; i++) {
+				const type = realm.create("TrainingType", {
+					_id: new BSON.ObjectId(),
+					name: "ベンチプレス",
+					part: "CHEST",
+				});
 
-			realm.create("Training", {
-				_id: new BSON.ObjectId(),
-				date: new Date(),
-				type,
-				sets: [
-					{ weight: 10, reps: 10, memo: "" },
-					{ weight: 20, reps: 20, memo: "" },
-					{ weight: 30, reps: 30, memo: "" },
-				],
-			});
+				realm.create("Training", {
+					_id: new BSON.ObjectId(),
+					type,
+					sets: [
+						{ weight: 10, reps: 10, memo: "" },
+						{ weight: 20, reps: 20, memo: "" },
+						{ weight: 30, reps: 30, memo: "" },
+					],
+				});
+			}
 		});
 	}, [realm]);
 
@@ -42,17 +49,17 @@ export const Home: React.FC = () => {
 						<List.Subheader style={styles.sectionHeader}>
 							今日のトレーニング
 						</List.Subheader>
-						{Array.from({ length: 30 }).map((_, i) => (
-							<View key={`key-${i}`}>
+						{trainings.map(({ _id, type, sets }) => (
+							<View key={_id.toString()}>
 								<List.Item
-									title="ベンチプレス"
-									description="5セット"
+									title={type.name}
+									description={`${sets.length}セット`}
 									right={(props) => (
 										<List.Icon {...props} icon="chevron-right" />
 									)}
 									titleStyle={{ fontWeight: "bold" }}
 									style={styles.listItem}
-									onPress={() => alert(`pressed ${i}`)}
+									onPress={() => alert(`pressed ${_id.toString()}`)}
 								/>
 								<Divider />
 							</View>
