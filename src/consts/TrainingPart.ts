07974bdf6ff347afc724a0ee4866d58c6cbf3e50
KazukiHayase <k.hayase@buysell-technologies.com>
2024-02-04 16:49:07 +0900

    update training type

--- src/consts/TrainingPart.ts
+++ src/consts/TrainingPart.ts
@@ -1,5 +1,20 @@
-export type TrainingPart = "chest";
+export type TrainingPart =
+	| "SHOULDER"
+	| "CHEST"
+	| "ARM"
+	| "BACK"
+	| "ABDOMEN"
+	| "LEG"
+	| "AEROBIC"
+	| "OTHER";
 
 export const TrainingPart: Record<TrainingPart, string> = {
-	chest: "胸",
+	SHOULDER: "肩",
+	CHEST: "胸",
+	ARM: "腕",
+	BACK: "背中",
+	ABDOMEN: "お腹",
+	LEG: "脚",
+	AEROBIC: "有酸素",
+	OTHER: "その他",
 };
