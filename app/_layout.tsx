ddd1aa7504615ad7b9be693d6cebd80748cbaea3
KazukiHayase <k.hayase@buysell-technologies.com>
2024-02-05 20:14:40 +0900

    move Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,53 @@
 import { LightTheme } from "@/style/theme";
-import { PaperProvider } from "react-native-paper";
+import { Icon, PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Navigation } from "@/components/Navigation";
 import "react-native-get-random-values";
 import { RealmProvider } from "@/realm";
+import { Tabs } from "expo-router";
+import dayjs from "dayjs";
 
 const Layout = () => {
 	// TODO: マスタデータの登録処理
+	const today = dayjs().locale("ja").format("YYYY/MM/DD");
 
 	return (
 		<SafeAreaProvider>
 			<PaperProvider theme={LightTheme}>
 				<RealmProvider>
-					<Navigation />
+					<Tabs initialRouteName="home">
+						<Tabs.Screen
+							name="index"
+							options={{
+								href: null,
+							}}
+						/>
+						<Tabs.Screen
+							name="home"
+							options={{
+								title: "ホーム",
+								tabBarIcon: ({ color, size }) => (
+									<Icon source="home" color={color} size={size} />
+								),
+								headerTitle: today,
+							}}
+						/>
+						<Tabs.Screen
+							name="record/index"
+							options={{
+								title: "記録",
+								tabBarIcon: ({ color, size }) => (
+									<Icon source="history" color={color} size={size} />
+								),
+							}}
+						/>
+						<Tabs.Screen
+							name="training/add/index"
+							options={{
+								title: "トレーニングを選択",
+								href: null,
+							}}
+						/>
+					</Tabs>
 				</RealmProvider>
 			</PaperProvider>
 		</SafeAreaProvider>
