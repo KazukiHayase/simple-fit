c993a3d09fd44af71ca68b01d3741cb214b63d84
KazukiHayase <k.hayase@buysell-technologies.com>
2024-06-27 19:12:24 +0900

    remove Paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,40 +1,36 @@
 import { SeedProvider } from "@/providers/SeedProvider";
 import { RealmProvider } from "@/realm";
-import { LightTheme } from "@/style/theme";
 import { config } from "@gluestack-ui/config";
 import { Box, GluestackUIProvider } from "@gluestack-ui/themed";
 import { Stack } from "expo-router";
 import "react-native-get-random-values";
-import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Layout = () => {
 	return (
 		<SafeAreaProvider>
 			<GluestackUIProvider config={config}>
-				<PaperProvider theme={LightTheme}>
-					<RealmProvider>
-						<SeedProvider>
-							<Box h="$full" bg="$red50">
-								<Stack initialRouteName="/tabs/home/index/">
-									<Stack.Screen
-										name="tabs"
-										options={{
-											headerShown: false,
-										}}
-									/>
-									<Stack.Screen
-										name="training/[id]"
-										options={{
-											title: "トレーニング",
-											headerBackTitle: "戻る",
-										}}
-									/>
-								</Stack>
-							</Box>
-						</SeedProvider>
-					</RealmProvider>
-				</PaperProvider>
+				<RealmProvider>
+					<SeedProvider>
+						<Box h="$full" bg="$red50">
+							<Stack initialRouteName="/tabs/home/index/">
+								<Stack.Screen
+									name="tabs"
+									options={{
+										headerShown: false,
+									}}
+								/>
+								<Stack.Screen
+									name="training/[id]"
+									options={{
+										title: "トレーニング",
+										headerBackTitle: "戻る",
+									}}
+								/>
+							</Stack>
+						</Box>
+					</SeedProvider>
+				</RealmProvider>
 			</GluestackUIProvider>
 		</SafeAreaProvider>
 	);
