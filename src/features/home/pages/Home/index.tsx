b5e0f7f6fb26734653c1fdc65d838b1c46b36389
KazukiHayase <k.hayase@buysell-technologies.com>
2024-06-27 20:46:25 +0900

    add router

--- src/features/home/pages/Home/index.tsx
+++ src/features/home/pages/Home/index.tsx
@@ -10,19 +10,29 @@ import {
 	HStack,
 	Heading,
 	Icon,
+	Pressable,
 	SafeAreaView,
 	Text,
 	VStack,
 } from "@gluestack-ui/themed";
 import dayjs from "dayjs";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 
 export const Home: React.FC = () => {
+	const router = useRouter();
+
 	const trainings = useQuery(Training, (trainings) => {
 		const today = dayjs().locale("ja").toDate();
 		today.setHours(0, 0, 0, 0);
 		return trainings.filtered("createdAt >= $0", today);
 	});
+
+	const handlePressTraining = (id: string) => {
+		router.push({
+			pathname: "/training/[id]",
+			params: { id },
+		});
+	};
 
 	return (
 		<SafeAreaView h="$full" pb="$0" bg="$white">
@@ -40,11 +50,12 @@ export const Home: React.FC = () => {
 					renderItem={({ item }) => {
 						const training = item as Training;
 						return (
-							<Box
+							<Pressable
 								py="$2"
 								px="$1"
 								borderTopWidth="$1"
 								borderColor="$borderLight100"
+								onPress={() => handlePressTraining(training._id.toString())}
 							>
 								<HStack
 									space="md"
@@ -61,7 +72,7 @@ export const Home: React.FC = () => {
 									</VStack>
 									<Icon as={ChevronRightIcon} size="md" color="$textLight500" />
 								</HStack>
-							</Box>
+							</Pressable>
 						);
 					}}
 				/>
